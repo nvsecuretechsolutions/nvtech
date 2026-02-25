389056ceb24c91dab4704cd2da7c3eec815723b1
nvsecuretechsolutions <nvsecuretechsolutions@gmail.com>
2026-02-25 14:40:22 +0530

    Added 404 redirect for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/nvtech">
       <div className="app-container">
         <Header />
         <div className="content-wrapper">
