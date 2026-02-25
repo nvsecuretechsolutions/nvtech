b1ba3206cfe46c0272dc60151b7cf4a9441e9d7e
nvsecuretechsolutions <nvsecuretechsolutions@gmail.com>
2026-02-25 14:51:46 +0530

    updated app.js file and modified 404 html file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './components/HomePage';
@@ -10,9 +10,25 @@ import MenuPage from './components/MenuPage';
 import SalesPage from './components/SalesPage';
 import './App.css';
 
+// Redirect handler component
+function RedirectHandler() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get('redirect');
+    if (redirect) {
+      navigate(redirect);
+    }
+  }, [navigate]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router basename="/nvtech">
+      <RedirectHandler />
       <div className="app-container">
         <Header />
         <div className="content-wrapper">
