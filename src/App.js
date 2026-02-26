d406ad24700120862cebe4827ae76f00619c6d2b
nvsecuretechsolutions <nvsecuretechsolutions@gmail.com>
2026-02-26 22:11:12 +0530

    updated app file contents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useNavigate } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
